b91eee3162dc30a463679f8b3a6ac44693a1c0d7
getzch <keepprogress@gmail.com>
2021-01-05 23:40:20 +0800

    delete model package add entity package and add new constructor to fix error

--- src/main/java/com/luv2code/springboot/thymeleafdemo/entity/Employee.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/entity/Employee.java
@@ -1,23 +1,45 @@
-package com.luv2code.springboot.thymeleafdemo.model;
+package com.luv2code.springboot.thymeleafdemo.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee")
 public class Employee {
-	
-	private int id;
-	private String firstName;
-	private String lastName;
-	private String email;
 
+	// define fields
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="email")
+	private String email;
+	
+	// define constructors
+	
 	public Employee() {
 		
 	}
 
-	public Employee(int id, String firstName, String lastName, String email) {
-		this.id = id;
+	public Employee(String firstName, String lastName, String email) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
 	}
 
+	// define getter/setter
+	
 	public int getId() {
 		return id;
 	}
@@ -50,11 +72,21 @@ public class Employee {
 		this.email = email;
 	}
 
+	// define tostring
+	
+	public Employee(int id, String firstName, String lastName, String email) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
 	@Override
 	public String toString() {
 		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
 	}
-	
+		
+
 	
 	
 }
